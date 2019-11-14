74d476f3d714437bde0ead97b9a75cc4fc786f0d
Abby Blachman <ablachman@Abbys-MacBook-Pro.local>
2019-11-14 14:03:57 -0600

    read csv package

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 const moment = require('moment');
+
+fs.createReadStream('legislators.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+  })
