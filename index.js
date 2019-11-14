e9456d98fb3c080577f9e5e56f17b404757b812c
Abby Blachman <ablachman@Abbys-MacBook-Pro.local>
2019-11-14 14:08:59 -0600

    age filter for democrats using moment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 const moment = require('moment');
+const results = [];
+const finalResults = [];
 
 fs.createReadStream('legislators.csv')
   .pipe(csv())
@@ -9,15 +11,18 @@ fs.createReadStream('legislators.csv')
     const democrats = results.filter(function(result) {
         return result.party = 'D';
       });
-      console.log(democrats);
-      const filteredAge = function(elements) {
-        elements.forEach(element => {
-          var bday = element.birthdate;
-          var bdayDate = moment(bday).format("L"); 
-          if (moment(bdayDate).isBetween('1974-11-01', '2019-11-01', 'year') === true) {
-          finalResults.push(element);
-          } else 
-          return; 
-        })
-      }
-  })
+    
+    const filteredAge = function(elements) {
+      elements.forEach(element => {
+        var bday = element.birthdate;
+        var bdayDate = moment(bday).format("L"); 
+        if (moment(bdayDate).isBetween('1974-11-01', '2019-11-01', 'year') === true) {
+        finalResults.push(element);
+        } else 
+        return; 
+      })
+      
+}
+filteredAge(democrats);
+console.log(finalResults);
+});
